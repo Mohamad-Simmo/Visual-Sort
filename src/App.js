78e5a969c91212247f35e02b7c6e114356d4f705
Mohammad Semmo <mohammadsemmo81@gmail.com>
2022-11-27 00:31:44 +0200

    Generate array

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const generateArray = (size) => {
+  return new Array(size).fill().map(() => Math.ceil(Math.random() * size));
+};
+
+const Bar = ({ height, width }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      className="array--bar"
+      style={{
+        minHeight: `${height}%`,
+        minWidth: `${width}%`,
+      }}
+    ></div>
+  );
+};
+
+const ArrayComponent = ({ size }) => {
+  const [arr, setArr] = useState([]);
+
+  useEffect(() => {
+    setArr(generateArray(size));
+  }, [size]);
+
+  const sort = () => {
+    setArr([...arr.sort((a, b) => a - b)]);
+  };
+
+  return (
+    <>
+      <div className="array--container">
+        {arr.map((num, idx) => (
+          <Bar
+            key={idx}
+            val={num}
+            height={(num / size) * 100}
+            width={100 / size}
+          />
+        ))}
+      </div>
+      <div className="array--actions">
+        <button className="btn" onClick={() => setArr(generateArray(size))}>
+          Reset
+        </button>
+        <button className="btn" onClick={sort}>
+          Sort
+        </button>
+      </div>
+    </>
+  );
+};
+
+function App() {
+  const [size, setSize] = useState(512);
+  return (
+    <div className="app">
+      <div className="form-control">
+        <label htmlFor="size">Size {size}</label>
+        <input
+          className="slider"
+          id="size"
+          min="2"
+          max="1024"
+          type="range"
+          value={size}
+          onChange={(e) => setSize(e.target.value)}
+        />
+      </div>
+      <ArrayComponent size={Number(size)} />
     </div>
   );
 }
